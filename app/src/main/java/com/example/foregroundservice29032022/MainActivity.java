36b6fb163f17901433b6fa86119a32d489f0960f
Phat <phatdroid94@gmail.com>
2022-06-25 19:18:00 +0700

    enable service

--- app/src/main/java/com/example/foregroundservice29032022/MainActivity.java
+++ app/src/main/java/com/example/foregroundservice29032022/MainActivity.java
@@ -36,5 +36,13 @@ public class MainActivity extends AppCompatActivity {
                 notificationManager.notify(1, notification);
             }
         });
+
+        btnStartForeground.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MyService.class);
+                startService(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/foregroundservice29032022/MyService.java
@@ -0,0 +1,29 @@
+package com.example.foregroundservice29032022;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class MyService extends Service {
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("BBB", "onCreate");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("BBB", "onStartCommand");
+        return super.onStartCommand(intent, flags, startId);
+    }
+}

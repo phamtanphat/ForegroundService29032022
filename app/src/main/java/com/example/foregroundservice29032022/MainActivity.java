853127246c8c8bdea8d28f2e2538ec542153f54f
Phat <phatdroid94@gmail.com>
2022-06-25 18:21:17 +0700

    notification basic

--- app/src/main/java/com/example/foregroundservice29032022/MainActivity.java
+++ app/src/main/java/com/example/foregroundservice29032022/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.foregroundservice29032022;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnCreateNotification, btnStartForeground, btnStopForeground;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCreateNotification = findViewById(R.id.button_create_notification);
+        btnStartForeground = findViewById(R.id.button_start_foreground_service);
+        btnStopForeground = findViewById(R.id.button_stop_foreground_service);
+
+        btnCreateNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/foregroundservice29032022/NotificationUtils.java
@@ -0,0 +1,18 @@
+package com.example.foregroundservice29032022;
+
+import android.app.Notification;
+import android.content.Context;
+
+import androidx.core.app.NotificationCompat;
+
+public class NotificationUtils {
+    private static String channel_id = "my_channel";
+
+    public static Notification createNotification(Context context, String title, String text) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, channel_id);
+        builder.setSmallIcon(android.R.drawable.star_big_on);
+        builder.setContentTitle(title);
+        builder.setContentText(text);
+        return builder.build();
+    }
+}

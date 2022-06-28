874ab291e630b4fc822ce62e6f936a06a7cc36be
Phat <phatdroid94@gmail.com>
2022-06-28 18:22:22 +0700

    setup notification

--- app/src/main/java/com/example/foregroundservice29032022/MyService.java
+++ app/src/main/java/com/example/foregroundservice29032022/MyService.java
@@ -1,6 +1,9 @@
 package com.example.foregroundservice29032022;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.IBinder;
@@ -8,11 +11,13 @@ import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 
 public class MyService extends Service {
 
+    boolean isPause = false;
+    Notification notification;
     int count = 0;
-
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -23,23 +28,13 @@ public class MyService extends Service {
     public void onCreate() {
         super.onCreate();
         Log.d("BBB", "onCreate");
+        notification = createNotification(count + "");
+        startForeground(1, notification);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d("BBB", "onStartCommand");
-        String text = "";
-        if (intent != null) {
-            text = intent.getStringExtra("text");
-        }
-        for (int i = 0; i < 1000; i++) {
-            try {
-                Thread.sleep(100);
-                Log.d("BBB",text + " " + i);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
         return START_STICKY;
     }
 
@@ -47,5 +42,23 @@ public class MyService extends Service {
     public void onDestroy() {
         super.onDestroy();
         Log.d("BBB", "onDestroy");
+    }
+
+    private Notification createNotification(String text) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(MyService.this, "my_channel");
+        builder.setSmallIcon(android.R.drawable.star_big_on);
+        builder.setContentTitle("My service is running");
+        builder.setContentText(text);
+
+        Intent intentOpenActivity = new Intent(MyService.this, MainActivity.class);
+        PendingIntent pendingOpenActivity = PendingIntent.getActivity(MyService.this, 0, intentOpenActivity, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Intent intentPause = new Intent(MyService.this, MainActivity.class);
+        intentPause.putExtra("isPause", true);
+        PendingIntent pendingPause = PendingIntent.getActivity(MyService.this, 0, intentOpenActivity, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        builder.setContentIntent(pendingOpenActivity);
+        builder.addAction(android.R.drawable.ic_media_pause, "Pause", pendingPause);
+        return builder.build();
     }
 }
